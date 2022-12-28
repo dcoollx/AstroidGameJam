73a49dade4e03653d2485b8ebfbd897097252bc3
David Queen <dsqjr@msn.com>
2022-12-28 17:59:35 -0500

    switched to phaser

--- scripts/main.js
+++ scripts/main.js
@@ -1,133 +1,81 @@
-const canvas = document.getElementById('game');
-const pen = canvas.getContext('2d');
-const gameOptions = {
-    height: 600,
-    width: 800,
-    maxSpeed: 50
-}
-console.log('setting up canvas')
-canvas.height = gameOptions.height;
-canvas.width = gameOptions.width;
-document.body.style.backgroundImage = 'url(./assets/background.jpg)'
-const gameObjects = [];
+const maxspeed = 200
+const level = 10
 
-
-class dynamicObject{
-    constructor(x=0, y=0, vx = 0, vy=0){
-        this.maxSpeed = gameOptions.maxSpeed
-        this.x = x;
-        this.y = y;
-        this.vy = vy;
-        this.vx = vx;
-        gameObjects.push(this)
-    }
-    setMaxSpeed(newSpeed){
-        this.maxSpeed = newSpeed
-    }
-    isOffScreen(){
-        return this.x > gameOptions.width || this.x < 0 || this.y > gameOptions.height || this.y < 0
-    }
-    update(){
-        // if(Math.abs(this.vx) > this.maxSpeed)
-        //     this.vx
-        this.x += this.vx;
-        this.y += this.vy;
-    }
-    setSprite(sprite){
-        this.sprite = sprite
-    }
-    draw(){
-        if(!this.sprite){
-            console.log('no sprite set')
-            return;
-        }
-        pen.drawImage(this.sprite, this.x, this.y)
-
-    }
-    destroy(){
-        delete this
-    }
-}
-class ship extends dynamicObject{
-    constructor(x=0, y=0, vx = 0, vy=0){
-        super(x,y,vx,vy);
-        this.rotation = 0;
-        this.rv = 0
-        console.log('player ready')
-        window.addEventListener('keydown',(e)=>{
-            if(e.code === 'a')
-                this.rv += -10
-            else if(e.code === 'd')
-                this.rv += 10
-            if(e.code === 'w'){
-                //move foward relative to rotation
-            }else if( e.code ==='d'){
-                //move backwards
-            }
-            if(e.code === 'space'){
-                this.fire();
-            }
-        });
-    }
-    fire(){
-        console.log('pew');
-    }
-    draw(){
-        pen.drawImage(this.sprite, this.x, this.y, gameOptions.width/10, gameOptions.height/10)
-    }
-
-}
-class astroid extends dynamicObject{
-    constructor(x=0, y=0, vx = 0, vy=0){
-        super(x,y,vx,vy);
-        this.Size = Math.floor(Math.random() * 10)
-    }
-    draw(){
-        pen.drawImage(this.sprite, this.x, this.y, 100,100)
-    }
-
-    hit(){
-        if(this.Size <= 1){
-            return this.destroy();
-        }
-    }
-
+function randomSpeed(){
+    return Phaser.Math.RND.between(-maxspeed, maxspeed)
 }
 
-function load(img){
-    const sprite = document.createElement('img');
-    sprite.src = img;
-    return sprite;
-
-}
-
-function setup(numberOfAstroids = 5){
-    const player = new ship(gameOptions.width/2, gameOptions.height/2);
-    console.log('player', player)
-    player.setSprite(load('./assets/spaceship.svg'))
-    const astroidSprite = load('./assets/astroid512x711.png')
-    for(let x = 0; x<= numberOfAstroids; x++){
-        // gen random numbers
-        let A = new astroid();
-        A.setSprite(astroidSprite)
-        A.x = Math.random() * gameOptions.width
-        A.y = Math.random() * gameOptions.height
-        A.vx = A.vy = Math.random()         
+function preload ()
+    {
+        //this.load.setBaseURL(window.location.href);
+        this.load.image('ship', './assets/spaceship.svg');
+        this.load.image('astroid', './assets/astroid512x711.png');
     }
-}
-
-function game(){
-    pen.clearRect(0, 0, canvas.width, canvas.height);
-    gameObjects.forEach(obj=>{
-        obj.update();
-        obj.draw();
-    })
-
-
+function create(){
+    console.log()
+    for(let x = 0; x <=level;x++){
+        test = this.physics.add.image(10,10, 'astroid')
+        test.setCollideWorldBounds(true).setBounce(1);
+        test.setVelocityX(randomSpeed())
+        test.setVelocityY(randomSpeed())
+        const scale = Math.random()/10
+        test.scaleX = scale
+        test.scaleY = scale
+    }
+    //test.setVelocityX(.random.realInRang(0,maxspeed))
+    console.log(Phaser.Math)
+    window.player = this.physics.add.image(113,150, 'ship')
+    player.setBounce(0.2);
+    player.setCollideWorldBounds(true);
+    //controls
     
 }
 
-console.log('starting game')
-console.time('first frame')
-setup();
-const continueGame = setInterval(game, 30/6000)
+
+var config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    physics: {
+        default: 'arcade',
+        arcade: {
+           
+        }
+    },
+    scene: {
+        preload: preload,
+        create: create,
+        update: function(){
+            cursors = this.input.keyboard.createCursorKeys();
+            if (cursors.left.isDown){
+                player.rotation += -0.07
+            }
+            else if (cursors.right.isDown){
+                player.rotation += 0.07  
+            } else {
+                player.setVelocityX(0);
+            }
+            if (cursors.up.isDown ){
+                const rotation = Phaser.Math.DegToRad
+                    (player.rotation)
+                const x = Math.cos(rotation)
+                const y = Math.tan(rotation);
+                console.log(x,y);
+                player.setVelocityX( maxspeed * x);
+                player.setVelocityY( maxspeed * -y);
+
+            }
+        }
+    }
+};
+
+var game = new Phaser.Game(config);
+
+
+
+
+
+
+
+
+
