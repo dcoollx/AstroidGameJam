788f7ab6429aaa09a47d46af210d2ab00a7258a0
David Queen <dsqjr@msn.com>
2022-12-29 01:18:43 -0500

    rotation works, added background

--- scripts/main.js
+++ scripts/main.js
@@ -9,20 +9,24 @@ function preload ()
     {
         //this.load.setBaseURL(window.location.href);
         this.load.image('ship', './assets/spaceship.svg');
+        this.load.image('space', './assets/background.jpg')
         this.load.image('astroid', './assets/astroid512x711.png');
+        this.load.audio('theme', './assets/am_i_nzxtinst.mp3')
     }
+      var astroids =[];
 function create(){
-    console.log()
+    this.add.image(config.width/2, config.width/2, 'space')
+
     for(let x = 0; x <=level;x++){
-        test = this.physics.add.image(10,10, 'astroid')
-        test.setCollideWorldBounds(true).setBounce(1);
-        test.setVelocityX(randomSpeed())
-        test.setVelocityY(randomSpeed())
-        const scale = Math.random()/10
-        test.scaleX = scale
-        test.scaleY = scale
+        astroids[x] = this.physics.add.image(10,10, 'astroid')
+        astroids[x].setCollideWorldBounds(true).setBounce(1);
+        astroids[x].setVelocityX(randomSpeed())
+        astroids[x].setVelocityY(randomSpeed())
+        const scale = Math.random()/10 + 0.05
+        astroids[x].scaleX = scale
+        astroids[x].scaleY = scale
     }
-    //test.setVelocityX(.random.realInRang(0,maxspeed))
+   
     console.log(Phaser.Math)
     window.player = this.physics.add.image(113,150, 'ship')
     player.setBounce(0.2);
@@ -34,8 +38,8 @@ function create(){
 
 var config = {
     type: Phaser.AUTO,
-    width: 800,
-    height: 600,
+    width: window.innerWidth,
+    height: window.innerHeight,
     physics: {
         default: 'arcade',
         arcade: {
@@ -46,23 +50,22 @@ var config = {
         preload: preload,
         create: create,
         update: function(){
+            for(astroid of astroids){
+                astroid.rotation += 0.005
+            }
             cursors = this.input.keyboard.createCursorKeys();
             if (cursors.left.isDown){
                 player.rotation += -0.07
             }
             else if (cursors.right.isDown){
                 player.rotation += 0.07  
-            } else {
-                player.setVelocityX(0);
-            }
+            } 
             if (cursors.up.isDown ){
-                const rotation = Phaser.Math.DegToRad
-                    (player.rotation)
-                const x = Math.cos(rotation)
-                const y = Math.tan(rotation);
-                console.log(x,y);
-                player.setVelocityX( maxspeed * x);
-                player.setVelocityY( maxspeed * -y);
+                const { rotation} = player
+                const x = Math.sin(rotation)
+                const y = Math.cos(rotation);
+                player.setVelocityY(-y * maxspeed);
+                player.setVelocityX(x * maxspeed);
 
             }
         }
